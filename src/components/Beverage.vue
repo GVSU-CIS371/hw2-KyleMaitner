8d0c575e5a64d2a3adc35a4851fc9372b6335a3c
KyleMaitner <maitnerk@mail.gvsu.edu>
2025-04-02 21:51:35 -0400

    Finial Update

--- src/components/Beverage.vue
+++ src/components/Beverage.vue
@@ -4,10 +4,10 @@
     <Hot v-else />
     <Contents>
       <template v-slot:top>
-        <Creamer />
+        <Creamer v-if="beverageStore.currentCreamer.id !== 'c1'" />
       </template>
       <template v-slot:mid>
-        <Syrup />
+        <Syrup v-if="beverageStore.currentSyrup.id !== 's1'" />
       </template>
       <template v-slot:bottom>
         <Base />
@@ -16,13 +16,17 @@
   </Mug>
 </template>
 <script setup lang="ts">
-import Contents from "./Contents.vue";
-import Mug from "./Mug.vue";
-import Syrup from "./Syrup.vue";
-import Base from "./Base.vue";
-import Creamer from "./Creamer.vue";
-import Hot from "./Hot.vue";
-import Cold from "./Cold.vue";
+import Contents from './Contents.vue';
+import Mug from './Mug.vue';
+import Syrup from './Syrup.vue';
+import Base from './Base.vue';
+import Creamer from './Creamer.vue';
+import Hot from './Hot.vue';
+import Cold from './Cold.vue';
+import {useBeverageStore } from "../stores/beverageStore";
+const beverageStore = useBeverageStore();
+
+
 type Props = {
   isIced: boolean;
 };
